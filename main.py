637213d349c279893d66e5789f40abe4caeffbfe
diego <diego.rodriguez.martinhotmail.com>
2026-05-21 21:46:02 +0200

    Añadir mensaje desde rama mejora-main

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ print ("Hola Git")
 print ("Este es mi segundo cambio")
 print ("Hoy estoy aprendiendo git diff")
 print ("Proyecto subido correctamente a GitHub")
-
+print ("Estoy aprendiendo a usar GitHub")
 
