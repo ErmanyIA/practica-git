3115a27bc45ee84938af9e69b1973315c4743d6e
diego <diego.rodriguez.martinhotmail.com>
2026-05-23 20:27:05 +0200

    Modificar saludo desde el main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print ("Hola Git")
+print ("Hola Git,desde la rama main")
 print ("Este es mi segundo cambio")
 print ("Hoy estoy aprendiendo git diff")
 print ("Proyecto subido correctamente a GitHub")
