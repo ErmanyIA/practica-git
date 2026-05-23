9920d3d7c4f8fd0b5eeb103729660952159817f0
diego <diego.rodriguez.martinhotmail.com>
2026-05-23 20:26:19 +0200

    Modificar saludo desde rama conflito

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print ("Hola Git")
+print ("Hola desde la rama conflicto-prueba")
 print ("Este es mi segundo cambio")
 print ("Hoy estoy aprendiendo git diff")
 print ("Proyecto subido correctamente a GitHub")
